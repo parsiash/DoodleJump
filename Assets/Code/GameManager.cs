27f3d4203bcffbe93d1afd98bacc9dcc16887b91
parsiash <parsia.shahini@gmail.com>
2021-02-19 13:37:57 +0330

    Game manager created

---
+++ Assets/Code/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using DoodleJump.Common;
+
+namespace DoodleJump
+{
+    public class GameManager : SingletonBehaviour<GameManager>
+    {
+        private Common.ILogger _logger;
+
+        public override void Init()
+        {
+            base.Init();
+            
+            _logger = new Logger();
+            _logger.Log("Game Manager Initialized");
+        }
+    }
+}
