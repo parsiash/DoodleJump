510b9fe96d47d99f58a86aa13c6f5c350ba2a502
parsiash <parsia.shahini@gmail.com>
2021-02-19 19:19:34 +0330

    Basic character controller implemented

---
+++ Assets/Code/Common/Entity.cs
@@ -0,0 +1,43 @@
+﻿using DoodleJump.Common;
+using UnityEngine;
+
+namespace DoodleJump.Common
+{
+    public interface IEntity
+    {
+        Vector2 Position { get; set; }
+        float ZIndex { get; set; }
+    }
+
+    public class Entity : CommonBehaviour, IEntity
+    {
+        public virtual Vector2 Position 
+        { 
+            get
+            {
+                return transform.position;
+            }
+
+            set
+            {
+                var currentPosition = transform.position;
+                transform.position = new Vector3(value.x, value.y, currentPosition.z);
+            }
+        }
+
+        public virtual float ZIndex
+        { 
+            get
+            {
+                return transform.position.z;
+            }
+
+            set
+            {
+                var position = transform.position;
+                position.z = value;
+                transform.position = position;
+            }
+        }
+    }
+}
---
+++ Assets/Code/Gameplay/CharacterController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DoodleJump.Common;
+using UnityEngine;
+
+namespace DoodleJump.Gameplay
+{
+    /// <summary>
+    /// The main script on the main jumping character.
+    /// </summary>
+    public class CharacterController : Entity
+    {
+        [SerializeField] private int jumpSpeed = 12;
+        [SerializeField] private float accelerationY = -20;
+        [SerializeField] private float moveSpeedX = 8;
+
+        private const float SCREEN_HALF_WIDTH = 3;
+
+        private Vector2 _speed;
+
+        public void InitializeForTheGame()
+        {
+            SetSpeed(Vector2.zero);
+        }
+
+        void Update()
+        {
+            SetSpeed(_speed + Vector2.up * accelerationY * Time.deltaTime);
+            ApplyMovement();
+
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                Jump();
+            }
+
+            // if(Input.GetKey(KeyCode.RightArrow))
+            // {
+            //     MoveX(moveSpeedX * Time.deltaTime);
+            // }else if(Input.GetKey(KeyCode.LeftArrow))
+            // {
+            //     MoveX(-moveSpeedX * Time.deltaTime);
+            // }
+
+            var horizontal = Input.GetAxis("Horizontal");
+            MoveX(horizontal * moveSpeedX * Time.deltaTime);
+        }
+
+        private void MoveX(float delta)
+        {
+            //move position by delta
+            var position = Position;
+            position += Vector2.right * delta;
+
+            //check screen limit
+            if(position.x > SCREEN_HALF_WIDTH)
+            {
+                position.x = -SCREEN_HALF_WIDTH;
+            }else if(position.x < -SCREEN_HALF_WIDTH)
+            {
+                position.x = SCREEN_HALF_WIDTH;
+            }
+
+            Position = position;
+        }
+
+        public void Jump()
+        {
+            SetSpeedY(jumpSpeed);
+        }
+
+        private void ApplyMovement()
+        {
+            Position += _speed * Time.deltaTime;
+        }
+
+        private void SetSpeedY(float speedY)
+        {
+            _speed.y = speedY;
+        }
+
+        private void SetSpeed(Vector2 speed)
+        {
+            _speed = speed;
+        }
+    }
+}
