632639f87d759703498ee2b17eeb16ed2557df7b
parsiash <parsia.shahini@gmail.com>
2021-02-20 22:19:58 +0330

    Basic test startup for GameplayController added to GameManager

--- Assets/Code/GameManager.cs
+++ Assets/Code/GameManager.cs
@@ -1,5 +1,7 @@
-﻿using DoodleJump.Common;
+﻿using System;
+using DoodleJump.Common;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace DoodleJump
 {
@@ -13,6 +15,21 @@ namespace DoodleJump
             
             _logger = new Common.Logger();
             _logger.Log("Game Manager Initialized");
+
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        private void OnSceneLoaded(Scene scebe, LoadSceneMode loadSceneMode)
+        {
+            var gameController = UnityEngine.Object.FindObjectOfType<Gameplay.GameplayController>();
+            var characterController  = UnityEngine.Object.FindObjectOfType<Gameplay.CharacterController>();
+            if(!gameController)
+            {
+                _logger.LogError($"Cannot start the game. No {nameof(Gameplay.GameplayController)} found.");
+                return;
+            }
+            
+            gameController.Initialize(null, characterController);
         }
 
         void Update()
